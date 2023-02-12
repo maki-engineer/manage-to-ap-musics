4713188d08667fe5b921d0f5f13c1aa00f409fea
maki-engineer <ryohei-81@outlook.jp>
2023-02-12 10:29:21 +0900

    add: 新しく追加された曲を追加するプログラムを実装

--- music_regist.js
+++ music_regist.js
@@ -3,3 +3,21 @@
 
 // もし出来そうだったらTwitterのapiを使用して自動で追加できるようにしてみたい
 // 無理なら手動で曲名をフルで入力して登録
+
+// SQLite3導入
+const sqlite3 = require("sqlite3");
+const db      = new sqlite3.Database("ap_musics.db");
+
+console.log("新しく追加された曲をフルで入力してエンターキーを押してください。");
+
+const reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.on("line", input => {
+  db.run("insert into musics(name) values(?)", input);
+  console.log("追加成功： " + input);
+
+  reader.close();
+});
